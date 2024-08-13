98e7d7b89a2f5ee46b765e55926fc1e8755349ec
TobiasWust <kontakt@tobiaswust.de>
2024-08-13 14:02:54 +0200

    fix color of logo

--- src/utils/useConsoleMsg.ts
+++ src/utils/useConsoleMsg.ts
@@ -5,6 +5,7 @@ export default function useConsoleMsg() {
     console.log("%cWell hello there. Looks like you're interested in what I do. Feel free to contact me. Just run contact()", "background-color: black; color: lime; padding: 5px 50px 5px 20px; font-family: 'Courier New'");
 
     (window as any).contact = function contact() {
+      console.log("%cNice. The best way to contact me is my contact form.", "background-color: black; color: lime; padding: 5px 50px 5px 20px; font-family: 'Courier New'");
       window.location.href = '/#contact'
     }
   }, []);
