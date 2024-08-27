465c90f6f93b2efe7f63b46e38d61a8a07c51cb2
TobiasWust <kontakt@tobiaswust.de>
2024-08-27 22:25:22 +0200

    use zustand persist

--- src/achievements/achievementStore.ts
+++ src/achievements/achievementStore.ts
@@ -2,21 +2,26 @@ import { toast } from 'react-toastify';
 import AchievementToast from '../components/AchievementToast';
 
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 type AchievementStore = {
   achievementIds: string[]
   addAchievement: (achievementId: string) => void;
 };
 
-const achievementStore = create<AchievementStore>((set) => ({
-  achievementIds: [],
-  addAchievement: (achievementId: string) => set((state) => {
-    console.log({ achievementId });
-    if (state.achievementIds.includes(achievementId)) return ({ achievementIds: state.achievementIds });
-    toast(() => AchievementToast({ achievementId }));
-    return ({ achievementIds: [...state.achievementIds, achievementId] })
+const achievementStore = create(
+  persist<AchievementStore>((set, get) => ({
+    achievementIds: [],
+    addAchievement: (achievementId: string) => {
+      if (get().achievementIds.includes(achievementId)) return;
+      toast(() => AchievementToast({ achievementId }));
+      set({ achievementIds: [...get().achievementIds, achievementId] })
+    }
+  }), {
+    name: 'achievementStore',
+    storage: createJSONStorage(() => localStorage)
   })
-}));
+);
 
 export default achievementStore;
 
