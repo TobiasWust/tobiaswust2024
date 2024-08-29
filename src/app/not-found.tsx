ad60341f1ae96e7f6ec083dfb8c52e09f1e24934
TobiasWust <kontakt@tobiaswust.de>
2024-08-29 16:35:30 +0200

    fix not-found mount

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,21 +3,28 @@
 import Link from 'next/link';
 import style from './not-found.module.scss'
 import achievementStore from '../achievements/achievementStore';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function NotFound() {
 
+  const [isMounted, setIsMounted] = useState(false);
   const addAchievement = achievementStore((state) => state.addAchievement);
 
   useEffect(() => {
-    addAchievement('404');
-  }, [addAchievement]);
+    setIsMounted(true);
+  }, [])
+
+  useEffect(() => {
+    if (isMounted) addAchievement('404');
+  }, [addAchievement, isMounted]);
 
   return (
     <div className={style.notfound}>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link className="link" href="/">Return Home</Link>
+      <div>
+        <h2>Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link className="link" href="/">Return Home</Link>
+      </div>
     </div>
   )
 }
